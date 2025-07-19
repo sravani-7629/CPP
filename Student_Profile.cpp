1f06bb12e7bcda57da515f17bf1ab3d71a1343b0
sravani-7629 <sravanipadmasri@gmail.com>
2025-07-19 10:43:48 +0530

    Add files via upload

---
+++ Student_Profile.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    int rollNo;
+    float marks;
+
+public:
+    string name;
+    string branch;
+
+    // Constructor to initialize all data
+    Student(int r, float m, string n, string b) {
+        rollNo = r;
+        marks = m;
+        name = n;
+        branch = b;
+    }
+
+    // Setter for marks
+    void setMarks(float m) {
+        marks = m;
+    }
+
+    // Getter for marks
+    float getMarks() {
+        return marks;
+    }
+
+    // Method to display student details
+    void displayDetails() {
+        cout << "Name: " << name << endl;
+        cout << "Branch: " << branch << endl;
+        cout << "Roll No: " << rollNo << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+
+
+int main() {
+    Student s1(61, 57.5, "Sravani", "ECE");
+    s1.displayDetails();
+
+    s1.setMarks(95.12);  
+    cout << "\nUpdated Marks: " << s1.getMarks() << endl;
+
+}
+
